6d69477d0bb3d7e7688de3f4151663bcba230501
Agatha_Ma <max995@126.com>
2019-10-09 21:00:20 +1100

    test result  bulid

--- PHP/logFile.php
+++ PHP/logFile.php
@@ -6,7 +6,7 @@
 		echo("Sent!");
 		echo("Field 1:".$text1);
 		echo("Field 2:".$text2);
-		$file = fopen("log.txt", "a");
+		$file = fopen("newlog.txt", "a");
 		fwrite($file, $text1);
 		fwrite($file, $text2);
 		fclose($file);
